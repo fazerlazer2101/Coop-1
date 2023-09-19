58c73ee1013c01d6887ae834cd047b85f805fd1f
Samuel <118148137+fazerlazer2101@users.noreply.github.com>
2023-09-19 10:52:54 -0500

    Base Foundations of the coopt with working fetch and image display

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.coopt_1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.coopt_1"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,11 +33,16 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+
 }
 
-dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+dependencies {
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/coopt_1/MainActivity.kt
+++ app/src/main/java/com/example/coopt_1/MainActivity.kt
@@ -1,11 +1,80 @@
 package com.example.coopt_1
 
-import androidx.appcompat.app.AppCompatActivity
+//Import these items
+
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import org.json.JSONArray
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Saves the button id to submitAPI.
+        /*val submitAPI: Button = findViewById(R.id.btn_searchForImage)*/
+
+        //Set on click event handler
+        /*submitAPI.setOnClickListener()
+        {
+            val resultTextView: TextView = findViewById((R.id.txt_HoldApi))
+            resultTextView.text = "Api";
+        }*/
+
+        /*submitAPI.setOnClickListener()
+        {
+            val resultTextView: TextView = findViewById((R.id.txt_HoldApi))
+            resultTextView.text = "Api";
+        };*/
+
+        findViewById<Button>(R.id.btn_searchForImage).setOnClickListener()
+        {
+            //Setting up Volley new request
+            val queue = Volley.newRequestQueue(this)
+            val url = "https://openlibrary.org/isbn/9780140328721.json"
+            
+
+            val resultTextView: TextView = findViewById((R.id.txt_HoldApi))
+
+            //Image items
+            val imageView:  ImageView = findViewById<ImageView>(R.id.img_Recieved)
+            //Retrieves jsonObject
+            val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+                { response ->
+
+
+                    //Parse JSON
+                    //Gets an array from json
+                    val responseJSON: JSONArray = response.getJSONArray("covers")
+
+                    //Sends to text view
+                    //Loops through array
+                    var i:Int = 0
+                    var imageId:String = ""
+                    while (i < responseJSON.length())
+                    {
+                        imageId = responseJSON.getString(i)
+                        resultTextView.text = responseJSON.getString(i)
+                        i++
+                    }
+
+
+                    //Loads recieved image
+                    Glide.with(this).load("https://covers.openlibrary.org/b/id/${imageId}-L.jpg").into(imageView)
+                },
+                { resultTextView.text = "That didn't work!" })
+
+            // Add the request to the RequestQueue.
+            queue.add(jsonObjectRequest)
+        }
     }
 }
